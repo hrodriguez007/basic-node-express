9621de74fd8baaeb06dc6259ba6640092bf08ed9
hrodriguez007 <heidyrodriguez567@gmail.com>
2023-10-16 14:40:25 -0500

    Add index html files instead of string

--- myApp.js
+++ myApp.js
@@ -3,16 +3,16 @@ let app = express();
 
 
 //#1 Challenge
-console.log("Hello World");
+// console.log("Hello World");
 
 //#2 Challenge
-app.get("/", (req, res) => {
-    res.send("Hello Express")
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello Express")
+// })
 
 //#3 Challenge
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + "/views/index.html");
 })
 
 
